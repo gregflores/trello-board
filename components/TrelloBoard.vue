978aa59662b7a7dd07b435f9be591de8a47feb05
Greg Flores <floresgreg@hotmail.com>
2025-03-02 17:30:25 -0600

    NUXT Start and Trello Start

---
+++ components/TrelloBoard.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { nanoid } from "nanoid";
+import type { Column, Task } from "~/types";
+
+const taskList = <Task[]>[
+	{
+		id: nanoid(),
+		title: "Create marketing landing page",
+		createdAt: new Date(),
+	},
+	{
+		id: nanoid(),
+		title: "Develop Cool new Feature",
+		createdAt: new Date(),
+	},
+	{
+		id: nanoid(),
+		title: "Fix Page Nav Bug",
+		createdAt: new Date(),
+	},
+];
+
+const columns = ref<Column[]>([
+	{
+		id: nanoid(),
+		title: "Backlog",
+		tasks: taskList,
+	},
+	{
+		id: nanoid(),
+		title: "In Progress",
+		tasks: [],
+	},
+	{
+		id: nanoid(),
+		title: "QA",
+		tasks: [],
+	},
+	{
+		id: nanoid(),
+		title: "Complete",
+		tasks: [],
+	},
+]);
+</script>
+
+<template>
+	<h1>Hello World</h1>
+</template>
